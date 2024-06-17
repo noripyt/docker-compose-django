0823dcc42713eff8052ff725a8b6b312b5e163b3
Bertrand Bordage <bordage.bertrand@gmail.com>
2024-06-17 18:56:42 +0200

    Caches templates in production.

--- django/example/settings/prod.py
+++ django/example/settings/prod.py
@@ -3,3 +3,15 @@ import os
 from .base import *
 
 ALLOWED_HOSTS = [os.environ['DOMAIN'], 'django']
+
+
+TEMPLATES[0]['OPTIONS']['loaders'] = [  # type: ignore[index]
+    (
+        'django.template.loaders.cached.Loader',
+        [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ],
+    ),
+]
+del TEMPLATES[0]['APP_DIRS']
