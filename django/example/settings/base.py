22bcc1037b1a50b5fee8deb6781901a6322c6ca8
Bertrand Bordage <bordage.bertrand@gmail.com>
2024-09-08 01:40:54 +0200

    Adds Memcached, but as mandatory (to be adjusted).

--- django/example/settings/base.py
+++ django/example/settings/base.py
@@ -95,6 +95,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': 'unix:/var/run/memcached/memcached.sock',
+        'KEY_PREFIX': 'example:',
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
